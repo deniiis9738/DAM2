806e37412e92d93cfa93f5848699ab7e34a5ced5
Denis <aluion9738@ieselcaminas.org>
2023-11-24 14:34:03 +0100

    v28

--- PROGRAMACION MULTIMEDIA Y DISPOSITIVOS MOVILES/Pokedex/app/build.gradle.kts
+++ PROGRAMACION MULTIMEDIA Y DISPOSITIVOS MOVILES/Pokedex/app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.6.0-alpha04")
     implementation ("androidx.compose.foundation:foundation:1.3.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("io.coil-kt:coil-compose:2.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- PROGRAMACION MULTIMEDIA Y DISPOSITIVOS MOVILES/Pokedex/app/src/main/java/com/example/pokedex/View/InfoPokemonView.kt
+++ PROGRAMACION MULTIMEDIA Y DISPOSITIVOS MOVILES/Pokedex/app/src/main/java/com/example/pokedex/View/InfoPokemonView.kt
@@ -1,15 +1,39 @@
 package com.example.pokedex.View
 
+import Pokemon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.example.pokedex.ViewModel.InfoPokemonViewModel
+import com.example.pokedex.ui.theme.Purple40
+import java.time.format.TextStyle
 
 class InfoPokemonView {
     @Composable
@@ -19,10 +43,67 @@ class InfoPokemonView {
 
         Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .background(Color.Gray),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            pokemon?.let { Text(text = it.weight.toString()) }
+            pokemon?.let { TopBar(it) }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(225.dp)
+                    .clip(RoundedCornerShape(bottomStart = 50.dp, bottomEnd = 50.dp))
+                    .background(Purple40),
+                contentAlignment = Alignment.Center
+            ) {
+                pokemon?.let { Image(
+                    painter = rememberAsyncImagePainter(model = it.sprites.other.officialArtwork.frontDefault),
+                    contentDescription = it.id,
+                    contentScale = ContentScale.Crop,
+                ) }
+            }
+            pokemon?.let { Text(
+                text = it.name,
+                fontSize = 40.sp,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(20.dp)
+            ) }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(pokemon: Pokemon) {
+    TopAppBar(
+        title = { Text(
+            text = "Pokedex",
+            color = Color.White,
+        ) },
+        modifier = Modifier
+            .fillMaxWidth(),
+        navigationIcon = {
+            IconButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "ArrowBack",
+                    tint = Color.White
+                )
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(Purple40),
+        actions = {
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = "#${pokemon.id}",
+                    color = Color.White
+                )
+            }
+        }
+    )
 }
